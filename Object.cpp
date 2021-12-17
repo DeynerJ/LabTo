55d2a9c87c4c64c5d59046ac1a07c63aa2bf86bb
Deyner Patiño <64093897+DeynerJ@users.noreply.github.com>
2021-12-17 01:55:02 -0500

    Update Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -2,44 +2,85 @@
 #include <string>
 using namespace std;
 
-class Toy {
+class Receta {
 protected:
 	string name;
 	float price;
+	int codigo;
+	int porciones;
+	string categoria;
 public:
-	virtual void prepareParts() = 0;
-	virtual void combineParts() = 0;
-	virtual void assembleParts() = 0;
-	virtual void applyLabel() = 0;
-	virtual void showProduct() = 0;
+	virtual void registrarPorciones()=0;
+	virtual void clasificarCategoria() =0;
+	virtual void cargarRaciones() = 0;
+	virtual void cargarIngredientes() = 0;
+	virtual void calcularPorciones() = 0;
+	virtual void registrandoPasos() = 0;
+	virtual void Presentar() = 0;
 };
 
-class Car : public Toy {
-//
+class Ceviche : public Receta {
+protected:
+	string name="Ceviche";
+	int porciones;
+	float price=0.5;
+	int codigo = 01;
+	string categoria="Peruano";
+	
 public:
-	void prepareParts() { cout << "Preparando partes de Car" << endl; };
-	void combineParts() { cout << "Combinando partes de Car" << endl; };
-	void assembleParts() { cout << "ensamblando partes de Car" << endl; };
-	void applyLabel() { cout << "aplicando etiquetado de Car" << endl; };
-	void showProduct() { cout << "Name: " <<name<< endl <<"Price: "<<price<<endl; };
+	void registrarPorciones(){ 
+		cout <<"Ingrese cantidad de Porciones: "<< endl;
+		cin >> porciones;
+		price *= porciones;
+	};
+	void clasificarCategoria() { cout << "Clasificando por Categoria: " << categoria << endl; };
+	void cargarRaciones() { cout << "Cantidad de Personas: " << porciones << endl; };
+	void cargarIngredientes() { cout << "Calculando los ingredientes" << endl; };
+	void calcularPorciones() { cout << "Calculando peso de ingredientes por cantidad de porciones" << endl; };
+	void registrandoPasos() { cout << "Calculando los pasos de acuerdo al peso de ingredientes "<<endl; };
+	void Presentar(){ cout <<"Nombre de receta: "<< name << endl <<"Precio: "<< price <<endl <<"Porciones: "<< porciones <<endl; };
 };
 
-class Bike : public Toy {
-	//
+class Chaufa : public Receta {
+protected:
+	string name="Chaufa";
+	int porciones;
+	float price=0.8;
+	int codigo = 02;
+	string categoria="Peruano";
+
 public:
-	void prepareParts() { cout << "Preparando partes de Bike" << endl; };
-	void combineParts() { cout << "Combinando partes de Bike" << endl; };
-	void assembleParts() { cout << "ensamblando partes de Bike" << endl; };
-	void applyLabel() { cout << "aplicando etiquetado de Bike" << endl; };
-	void showProduct() { cout << "Name: " << name << endl << "Price: " << price << endl; };
+	void registrarPorciones(){ 
+		cout <<"Ingrese cantidad de Porciones: "<< endl;
+		cin >> porciones;
+		price *= porciones;;
+	};
+	void clasificarCategoria() { cout << "Clasificando por Categoria" << categoria << endl; };
+	void cargarRaciones() { cout << "Cantidad de Personas: " << porciones << endl; };
+	void cargarIngredientes() { cout << "Calculando los ingredientes" << endl; };
+	void calcularPorciones() { cout << "Calculando peso de ingredientes por cantidad de porciones" << endl; };
+	void registrandoPasos() { cout << "Calculando los pasos de acuerdo al peso de ingredientes "<<endl; };
+	void Presentar(){ cout <<"Nombre de receta: "<< name << endl <<"Precio: "<< price <<endl <<"Porciones: "<< porciones <<endl; };
 };
 
-class Plane : public Toy {
-	//
+class Sopa : public Receta {
+protected:
+	string name="Ceviche";
+	int porciones;
+	float price=0.10;
+	int codigo = 03;
+	string categoria="Peruano";
+	
 public:
-	void prepareParts() { cout << "Preparando partes de Plane" << endl; };
-	void combineParts() { cout << "Combinando partes de Plane" << endl; };
-	void assembleParts() { cout << "ensamblando partes de Plane" << endl; };
-	void applyLabel() { cout << "aplicando etiquetado de Plane" << endl; };
-	void showProduct() { cout << "Name: " << name << endl << "Price: " << price << endl; };
+	void registrarPorciones(){ 
+		cout <<"Ingrese cantidad de Porciones: "<< endl;
+		cin >> porciones;
+		price *= porciones;
+	};
+	void clasificarCategoria() { cout << "Clasificando por Categoria" <<categoria << endl; };
+	void cargarRaciones() { cout << "Cantidad de Personas: " << porciones << endl; };
+	void cargarIngredientes() { cout << "Calculando los ingredientes" << endl; };
+	void calcularPorciones() { cout << "Calculando peso de ingredientes por cantidad de porciones" << endl; };
+	void registrandoPasos() { cout << "Calculando los pasos de acuerdo al peso de ingredientes "<<endl; };
+	void Presentar(){ cout <<"Nombre de receta: "<< name << endl <<"Precio: "<< price <<endl <<"Porciones: "<< porciones <<endl; };
 };
